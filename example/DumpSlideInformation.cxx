e4b518d7713d1251c704d241486249b7faf949f3
Matt McCormick <matt.mccormick@kitware.com>
2016-05-01 21:51:38 -0400

    STYLE: Follow ITK Style Guidelines in the example.
    See www.itk.org/Wiki/ITK/Coding_Style_Guide.

--- example/DumpSlideInformation.cxx
+++ example/DumpSlideInformation.cxx
@@ -10,28 +10,34 @@
 #include "itkRGBAPixel.h"
 #include "itkMetaDataObject.h"
 
-void Usage(const char *p_cArg0) {
-  std::cerr << "Usage: " << p_cArg0 << " slideFile" << std::endl;
+void Usage(const char *cArg0)
+{
+  std::cerr << "Usage: " << cArg0 << " slideFile" << std::endl;
   exit(1);
 }
 
-class DumpSlideInformation {
+
+class DumpSlideInformation
+{
 public:
   typedef itk::RGBAPixel<unsigned char> PixelType;
-  typedef itk::Image<PixelType, 2> ImageType;
-  typedef itk::OpenSlideImageIO ReaderIOType;
+  typedef itk::Image<PixelType, 2>      ImageType;
+  typedef itk::OpenSlideImageIO         ReaderIOType;
 
-  DumpSlideInformation() {
-    m_p_clReaderIO = ReaderIOType::New();
+  DumpSlideInformation()
+  {
+    m_CLReaderIO = ReaderIOType::New();
   }
 
   // Don't try to load an image larger than this size
-  size_t MaxImageSizeInBytes() const {
+  size_t MaxImageSizeInBytes() const
+  {
     return (size_t)1024 * 1024 * 100;
   }
 
-  void SetFileName(const std::string &strFileName) {
-    m_p_clReaderIO->SetFileName(strFileName);
+  void SetFileName(const std::string & fileName)
+  {
+    m_CLReaderIO->SetFileName(fileName);
   }
 
   bool DumpInformation() const;
@@ -39,31 +45,36 @@ public:
   bool WriteAssociatedImages() const;
 
 private:
-  ReaderIOType::Pointer m_p_clReaderIO;
+  ReaderIOType::Pointer m_CLReaderIO;
 
-  bool ReadImageInformation() const {
-    try {
-      m_p_clReaderIO->ReadImageInformation();
-    }
-    catch (itk::ExceptionObject &e) {
+  bool ReadImageInformation() const
+  {
+    try
+      {
+      m_CLReaderIO->ReadImageInformation();
+      }
+    catch (itk::ExceptionObject & e)
+      {
       std::cerr << "Error: " << e << std::endl;
       return false;
-    }
+      }
 
     return true;
   }
 
-  ImageType::SizeType GetSize() const {
-    ImageType::SizeType clSize;  
-    clSize[0] = m_p_clReaderIO->GetDimensions(0);
-    clSize[1] = m_p_clReaderIO->GetDimensions(1);
+  ImageType::SizeType GetSize() const
+  {
+    ImageType::SizeType clSize;
+    clSize[0] = m_CLReaderIO->GetDimensions(0);
+    clSize[1] = m_CLReaderIO->GetDimensions(1);
     return clSize;
   }
 
-  ImageType::SpacingType GetSpacing() const {
+  ImageType::SpacingType GetSpacing() const
+  {
     ImageType::SpacingType clSpacing;
-    clSpacing[0] = m_p_clReaderIO->GetSpacing(0);
-    clSpacing[1] = m_p_clReaderIO->GetSpacing(1);
+    clSpacing[0] = m_CLReaderIO->GetSpacing(0);
+    clSpacing[1] = m_CLReaderIO->GetSpacing(1);
     return clSpacing;
   }
 
@@ -72,130 +83,172 @@ private:
   bool DumpLevelInformation() const;
   bool DumpAssociatedImageInformation() const;
 
-  bool WriteImage(const char *p_cFileName) const;
+  bool WriteImage(const char * fileName) const;
 };
 
 
-int main(int argc, char **argv) {
-  const char * const p_cArg0 = argv[0];
+int main(int argc, char **argv)
+{
+  const char * const cArg0 = argv[0];
   if (argc != 2)
-    Usage(p_cArg0);
+    {
+    Usage(cArg0);
+    return EXIT_FAILURE;
+    }
 
-  std::string strSlideImage = argv[1];
+  std::string slideImage = argv[1];
 
   DumpSlideInformation clInfo;
-  clInfo.SetFileName(strSlideImage);
+  clInfo.SetFileName(slideImage);
 
   if (!clInfo.DumpInformation() || !clInfo.WriteLevels() || !clInfo.WriteAssociatedImages())
-    return -1;
+    {
+    return EXIT_FAILURE;
+    }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
 
-bool DumpSlideInformation::DumpInformation() const {
+
+bool
+DumpSlideInformation
+::DumpInformation() const
+{
   return ReadImageInformation() && DumpImageInformation() && DumpMetaData() && DumpLevelInformation() && DumpAssociatedImageInformation();
 }
 
-bool DumpSlideInformation::DumpImageInformation() const {
+
+bool
+DumpSlideInformation
+::DumpImageInformation() const
+{
   std::cout << "\nImage Information:\n" << std::endl;
-  std::cout << "Dimensions: " << m_p_clReaderIO->GetNumberOfDimensions() << std::endl;
-  std::cout << "Component type: " << itk::ImageIOBase::GetComponentTypeAsString(m_p_clReaderIO->GetComponentType()) << std::endl;
-  std::cout << "Pixel type: " << itk::ImageIOBase::GetPixelTypeAsString(m_p_clReaderIO->GetPixelType()) << std::endl;
-  std::cout << "Vendor: " << m_p_clReaderIO->GetVendor() << std::endl;
+  std::cout << "Dimensions: " << m_CLReaderIO->GetNumberOfDimensions() << std::endl;
+  std::cout << "Component type: " << itk::ImageIOBase::GetComponentTypeAsString(m_CLReaderIO->GetComponentType()) << std::endl;
+  std::cout << "Pixel type: " << itk::ImageIOBase::GetPixelTypeAsString(m_CLReaderIO->GetPixelType()) << std::endl;
+  std::cout << "Vendor: " << m_CLReaderIO->GetVendor() << std::endl;
 
   return true;
 }
 
-bool DumpSlideInformation::DumpMetaData() const {
+
+bool
+DumpSlideInformation
+::DumpMetaData() const
+{
   std::cout << "\nMeta Data:\n" << std::endl;
 
-  itk::MetaDataDictionary &clTags = m_p_clReaderIO->GetMetaDataDictionary();
-  std::vector<std::string> vKeys = clTags.GetKeys();
+  itk::MetaDataDictionary &clTags = m_CLReaderIO->GetMetaDataDictionary();
+  std::vector<std::string> keys = clTags.GetKeys();
 
-  std::cout << "Number of keys: " << vKeys.size() << std::endl;
+  std::cout << "Number of keys: " << keys.size() << std::endl;
   std::cout << "Entries:" << std::endl;
-  for (size_t i = 0; i < vKeys.size(); ++i) {
-    const std::string &strKey = vKeys[i];
-    std::string strValue;
+  for (size_t i = 0; i < keys.size(); ++i)
+    {
+    const std::string & key = keys[i];
+    std::string value;
 
-    if (itk::ExposeMetaData(clTags, strKey, strValue))
-      std::cout << strKey << " = " << strValue << std::endl;
-  }
+    if (itk::ExposeMetaData(clTags, key, value))
+      {
+      std::cout << key << " = " << value << std::endl;
+      }
+    }
 
   return true;
 }
 
-bool DumpSlideInformation::DumpLevelInformation() const {
+
+bool
+DumpSlideInformation
+::DumpLevelInformation() const
+{
   std::cout << "\nLevel Information:\n" << std::endl;
 
-  const int iLevelCount = m_p_clReaderIO->GetLevelCount();
-  std::cout << "Level count: " << iLevelCount << std::endl;
+  const int levelCount = m_CLReaderIO->GetLevelCount();
+  std::cout << "Level count: " << levelCount << std::endl;
 
   std::cout << "Levels:" << std::endl;
-  for (int iLevel = 0; iLevel < iLevelCount; ++iLevel) {
-    m_p_clReaderIO->SetLevel(iLevel);
+  for (int level = 0; level < levelCount; ++level)
+    {
+    m_CLReaderIO->SetLevel(level);
 
     if (!ReadImageInformation())
+      {
       return false;
+      }
 
-    std::cout << "Level " << iLevel << ": dimensions = " << GetSize() << ", spacing = " << GetSpacing() << ", size in bytes = " << m_p_clReaderIO->GetImageSizeInBytes() << std::endl;
-  }
+    std::cout << "Level " << level << ": dimensions = " << GetSize() << ", spacing = " << GetSpacing() << ", size in bytes = " << m_CLReaderIO->GetImageSizeInBytes() << std::endl;
+    }
 
   return true;
 }
 
-bool DumpSlideInformation::DumpAssociatedImageInformation() const {
+
+bool
+DumpSlideInformation
+::DumpAssociatedImageInformation() const
+{
   std::cout << "\nAssociated image information:\n" << std::endl;
 
-  ReaderIOType::AssociatedImageNameContainer vAssociatedImages = m_p_clReaderIO->GetAssociatedImageNames();
+  ReaderIOType::AssociatedImageNameContainer associatedImages = m_CLReaderIO->GetAssociatedImageNames();
 
-  std::cout << "Number of associated images: " << vAssociatedImages.size() << std::endl;
+  std::cout << "Number of associated images: " << associatedImages.size() << std::endl;
   std::cout << "Associated image names:" << std::endl;
 
   const size_t numWordsPerLine = 3;
 
-  for (size_t i = 0; i < vAssociatedImages.size(); i += numWordsPerLine) {
+  for (size_t i = 0; i < associatedImages.size(); i += numWordsPerLine)
+    {
     const size_t jBegin = i;
-    const size_t jEnd = std::min(vAssociatedImages.size(), jBegin + numWordsPerLine);
+    const size_t jEnd = std::min(associatedImages.size(), jBegin + numWordsPerLine);
 
-    std::cout << '\'' << vAssociatedImages[jBegin] << '\'';
+    std::cout << '\'' << associatedImages[jBegin] << '\'';
 
     for (size_t j = jBegin+1; j < jEnd; ++j)
-      std::cout << ", '" << vAssociatedImages[j] << '\'';
+      {
+      std::cout << ", '" << associatedImages[j] << '\'';
+      }
 
     std::cout << std::endl;
-  }
+    }
 
   std::cout << "\nAssociated images:" << std::endl;
 
-  for (size_t i = 0; i < vAssociatedImages.size(); ++i) {
-    const std::string &strAssociatedImage = vAssociatedImages[i];
+  for (size_t i = 0; i < associatedImages.size(); ++i)
+    {
+    const std::string &associatedImage = associatedImages[i];
 
-    m_p_clReaderIO->SetAssociatedImageName(strAssociatedImage);
+    m_CLReaderIO->SetAssociatedImageName(associatedImage);
 
-    if (!ReadImageInformation())
+    if (!this->ReadImageInformation())
+      {
       return false;
+      }
 
-    std::cout << strAssociatedImage << ": dimensions = " << GetSize() << ", spacing = " << GetSpacing() << ", size in bytes = " << m_p_clReaderIO->GetImageSizeInBytes() << std::endl;
-  }
+    std::cout << associatedImage << ": dimensions = " << GetSize() << ", spacing = " << GetSpacing() << ", size in bytes = " << m_CLReaderIO->GetImageSizeInBytes() << std::endl;
+    }
 
   return true;
 }
 
-bool DumpSlideInformation::WriteLevels() const {
+
+bool
+DumpSlideInformation
+::WriteLevels() const
+{
   std::cout << "\nWriting level images to file ...\n" << std::endl;
 
-  const int iLevelCount = m_p_clReaderIO->GetLevelCount();
+  const int iLevelCount = m_CLReaderIO->GetLevelCount();
 
   std::stringstream nameStream;
 
   for (int iLevel = 0; iLevel < iLevelCount; ++iLevel) {
-    m_p_clReaderIO->SetLevel(iLevel);
+    m_CLReaderIO->SetLevel(iLevel);
 
     if (!ReadImageInformation())
       return false;
 
-    if (m_p_clReaderIO->GetImageSizeInBytes() > MaxImageSizeInBytes()) {
+    if (m_CLReaderIO->GetImageSizeInBytes() > MaxImageSizeInBytes()) {
       std::cout << "Level " << iLevel << " image is too large. Skipping." << std::endl;
       continue;
     }
@@ -213,57 +266,70 @@ bool DumpSlideInformation::WriteLevels() const {
   return true;
 }
 
-bool DumpSlideInformation::WriteAssociatedImages() const {
+
+bool
+DumpSlideInformation
+::WriteAssociatedImages() const
+{
   std::cout << "\nWriting associated images to file ...\n" << std::endl;
 
-  ReaderIOType::AssociatedImageNameContainer vAssociatedImages = m_p_clReaderIO->GetAssociatedImageNames();
+  ReaderIOType::AssociatedImageNameContainer associatedImages = m_CLReaderIO->GetAssociatedImageNames();
 
-  for (size_t i = 0; i < vAssociatedImages.size(); ++i) {
-    const std::string &strAssociatedImage = vAssociatedImages[i];
+  for (size_t i = 0; i < associatedImages.size(); ++i)
+    {
+    const std::string & associatedImage = associatedImages[i];
 
-    m_p_clReaderIO->SetAssociatedImageName(strAssociatedImage);
+    m_CLReaderIO->SetAssociatedImageName(associatedImage);
 
-    if (!ReadImageInformation())
+    if (!this->ReadImageInformation())
+      {
       return false;
+      }
 
-    if (m_p_clReaderIO->GetImageSizeInBytes() > MaxImageSizeInBytes()) {
-      std::cout << "Associated image '" << strAssociatedImage << "' is too large. Skipping." << std::endl;
+    if (m_CLReaderIO->GetImageSizeInBytes() > MaxImageSizeInBytes())
+      {
+      std::cout << "Associated image '" << associatedImage << "' is too large. Skipping." << std::endl;
       continue;
-    }
+      }
 
-    std::string strFileName = strAssociatedImage + ".tiff";
+    std::string fileName = associatedImage + ".tiff";
 
-    std::cout << "Writing associated image '" << strAssociatedImage << "' to '" << strFileName << "' ..." << std::endl;
+    std::cout << "Writing associated image '" << associatedImage << "' to '" << fileName << "' ..." << std::endl;
 
-    WriteImage(strFileName.c_str());
+    this->WriteImage(fileName.c_str());
   }
 
   return true;
 }
 
-bool DumpSlideInformation::WriteImage(const char *p_cFileName) const {
+
+bool
+DumpSlideInformation
+::WriteImage(const char *fileName) const
+{
   typedef itk::ImageFileReader<ImageType> ReaderType;
   typedef itk::ImageFileWriter<ImageType> WriterType;
- 
-  ReaderType::Pointer p_clReader = ReaderType::New();
 
-  const std::string strInputFileName = m_p_clReaderIO->GetFileName();
+  ReaderType::Pointer clReader = ReaderType::New();
 
-  p_clReader->SetImageIO(m_p_clReaderIO);
-  p_clReader->SetFileName(strInputFileName);
-  
-  WriterType::Pointer p_clWriter = WriterType::New();
-  p_clWriter->SetInput(p_clReader->GetOutput());
-  p_clWriter->SetFileName(p_cFileName);
+  const std::string strInputFileName = m_CLReaderIO->GetFileName();
 
-  try {
-    p_clWriter->Update();
-  }
-  catch (itk::ExceptionObject &e) {
+  clReader->SetImageIO(m_CLReaderIO);
+  clReader->SetFileName(strInputFileName);
+
+  WriterType::Pointer clWriter = WriterType::New();
+  clWriter->SetInput(clReader->GetOutput());
+  clWriter->SetFileName(fileName);
+
+  try
+    {
+    clWriter->Update();
+    }
+  catch (itk::ExceptionObject & e)
+    {
     std::cerr << "Error: " << e << std::endl;
     return false;
-  }
+    }
 
   return true;
 }
-
