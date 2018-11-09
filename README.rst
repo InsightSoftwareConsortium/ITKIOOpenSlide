c8f14d3bdfa46272c347666fb93b240204b31dbb
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-11-08 20:26:03 -0500

    ENH: Add Circle and AppVeyor CI.
    Add `CircleCI` and `AppVeyor` CI:
    - Add the necessary `*.yml` configuration files.
    - Add the Python `setup.py`file.
    - Add CI badges to the `README.rst` file.
    - Take advantage of the commit to add the license notice to the
      `README.rst` file.

--- README.rst
+++ README.rst
@@ -1,12 +1,27 @@
-=====================
 ITKIOOpenSlide
-=====================
+==============
 
-.. image:: https://circleci.com/gh/InsightSoftwareConsortium/ITKIOOpenSlide.svg?style=svg
+.. |CircleCI| image:: https://circleci.com/gh/InsightSoftwareConsortium/ITKIOOpenSlide.svg?style=shield
     :target: https://circleci.com/gh/InsightSoftwareConsortium/ITKIOOpenSlide
 
+.. |TravisCI| image:: https://travis-ci.org/InsightSoftwareConsortium/ITKIOOpenSlide.svg?branch=master
+    :target: https://travis-ci.org/InsightSoftwareConsortium/ITKIOOpenSlide
+
+.. |AppVeyor| image:: https://img.shields.io/appveyor/ci/itkrobot/itkioopenslide.svg
+    :target: https://ci.appveyor.com/project/itkrobot/itkioopenslide
+
+=========== =========== ===========
+   Linux      macOS       Windows
+=========== =========== ===========
+|CircleCI|  |TravisCI|  |AppVeyor|
+=========== =========== ===========
+
+
+Overview
+--------
+
 This is an InsightToolkit (ITK) ExternalModule to provide an ImageIO for the
-OpenSlide library supported file formats.  These are generally TIFF-based
+OpenSlide library supported file formats. These are generally TIFF-based
 microscopy formats.
 
 To use this module, clone it into the Modules/External directory in the ITK
@@ -15,3 +30,10 @@ source tree.
 Note that the OpenSlide library (http://openslide.org) must be available.
 
 *Note:*  This module is currently a work in progress.
+
+
+License
+-------
+
+This software is distributed under the Apache 2.0 license. Please see
+the *LICENSE* file for details.
---
+++ setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from os import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('scikit-build is required to build from source.', file=sys.stderr)
+    print('Please run:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  python -m pip install scikit-build')
+    sys.exit(1)
+
+setup(
+    name='itk-ioopenslide',
+    version='0.1.0',
+    author='Matthew McCormick',
+    author_email='matt.mccormick@kitware.com',
+    packages=['itk'],
+    package_dir={'itk': 'itk'},
+    download_url=r'https://github.com/InsightSoftwareConsortium/ITKIOOpenSlide',
+    description=r"ImageIO for the OpenSlide library supported file formats.",
+    long_description='ITKIOOpenSlide provides an ImageIO for the OpenSlide'
+                     'library supported file formats. These are generally'
+                     'TIFF-based microscopy formats.',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: Android",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS"
+        ],
+    license='Apache',
+    keywords='ITK InsightToolkit',
+    url=r'https://itk.org/',
+    install_requires=[
+        r'itk'
+    ]
+    )
