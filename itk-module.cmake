3ccba417139821699a521efc0b8905bc6c698b53
Dženan Zukić <dzenan.zukic@kitware.com>
2025-03-25 14:59:54 -0400

    ENH: Add undeclared test dependency on MetaIO

--- itk-module.cmake
+++ itk-module.cmake
@@ -2,15 +2,17 @@ set( DOCUMENTATION "This module contains ImageIO classes for reading microscopy
 images (mostly TIFF based) readable by the OpenSlide library,
 https://openslide.org.")
 
-itk_module( IOOpenSlide
+itk_module(IOOpenSlide
   DEPENDS
     ITKIOImageBase
   TEST_DEPENDS
     ITKTestKernel
+    ITKIOMeta
   ENABLE_SHARED
   FACTORY_NAMES
     ImageIO::OpenSlide
   DESCRIPTION
     "${DOCUMENTATION}"
   EXCLUDE_FROM_DEFAULT
+  ENABLE_SHARED
 )
